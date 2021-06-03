da27d53a455d4bc04d1a1791c38a270a7b59cf1b
gjj2828 <gjj2828@gmail.com>
2021-06-04 00:21:39 +0800

    测试 Instantiate 会复制哪些信息

---
+++ Assets/Scripts/TestMgr.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestMgr : MonoBehaviour
+{
+    private GameObject unit1;
+    private GameObject unit2;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        unit1 = new GameObject("TestUnit1", typeof(TestUnit));
+        StartCoroutine(TestProcess());
+    }
+
+    private IEnumerator TestProcess()
+    {
+        yield return new WaitForSeconds(1.0f);
+        unit1.GetComponent<TestUnit>().SetFlag(true);
+        yield return new WaitForSeconds(1.0f);
+        unit2 = Instantiate(unit1);
+        unit2.name = "TestUnit2";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/TestUnit.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestUnit : MonoBehaviour
+{
+    private bool flag = false;
+    public bool flag_public = false;
+    [SerializeField]
+    private bool flag_serialize = false;
+    [HideInInspector]
+    public bool flag_hide = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        LogFlag();
+    }
+
+    public void SetFlag(bool f)
+    {
+        flag = f;
+        flag_public = f;
+        flag_serialize = f;
+        flag_hide = f;
+        LogFlag();
+    }
+
+    private void LogFlag()
+    {
+        Debug.Log(string.Format("{0}: {1}, {2}, {3}, {4}", name, flag, flag_public, flag_serialize, flag_hide));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
